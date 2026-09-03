8335a0a0dfd14b6a52ddb3ba0bb9e85c1951a20c
_daephoria <Emerlajsan@yahoo.com>
2026-09-04 12:38:07 +1300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+studname = "Ching" #string
+studage = 14 #integer
+stud_height = 170 #integer
+stud_country_to_visit = ["South Korea, Switzerland, Maldives"] #list
+stud_new = False #boolean
+stud_info = {"color" :"black and purple",
+            "car_brand": "Porche",
+            "shoe_size" :"10",
+            "best_friend" :"Youngeun"
+            } #dictionary
+stud_favefruits = {
+    "banana",
+    "watermelon",
+    "grapes",
+    "rambutan",
+    "mango"
+} #list
+days_of_week = (
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+) #list
+
+print("name:", studname)
+print("age:", studage)
+print("height:", stud_height)
+print("countries to visit:", stud_country_to_visit)
+print("New student:", stud_new)
+print("Student Info:", stud_info)
+print("Favorite Fruits:", stud_favefruits)
+print("Days of the Week:", days_of_week)
